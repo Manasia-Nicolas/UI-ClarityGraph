f75761819db2e9659d9580785538578ab2ceec65
Anonymous <anon@example.com>
2025-11-27 09:27:50 +0100

    Initial Qt project

---
+++ build/Qt_6_10_1_for_macOS-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_UI-ClarityGraph_FILE /Users/nicolas/Desktop/Proiecte/C++/UI-ClarityGraph/build/Qt_6_10_1_for_macOS-Debug/UI-ClarityGraph.app/Contents/MacOS/UI-ClarityGraph)
+set(__QT_DEPLOY_TARGET_UI-ClarityGraph_TYPE EXECUTABLE)
